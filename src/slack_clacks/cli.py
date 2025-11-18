081fea2a558612dc2b371a7532a34a09c78d8648
Neeraj Kashyap <nkashy1@gmail.com>
2025-11-17 23:08:23 -0800

    reformat

--- src/slack_clacks/cli.py
+++ src/slack_clacks/cli.py
@@ -2,6 +2,7 @@ import argparse
 
 from slack_clacks.configuration.cli import generate_cli as generate_init_cli
 
+
 def generate_cli() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description="clacks: Control Slack from your command line"
@@ -10,6 +11,8 @@ def generate_cli() -> argparse.ArgumentParser:
     subparsers = parser.add_subparsers()
 
     init_parser = generate_init_cli()
-    subparsers.add_parser("init", parents=[init_parser], add_help=False, help=init_parser.description)
+    subparsers.add_parser(
+        "init", parents=[init_parser], add_help=False, help=init_parser.description
+    )
 
     return parser
