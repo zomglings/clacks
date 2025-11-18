6b0fd394b44a472dce06f01fd3213d236a284866
Neeraj Kashyap <nkashy1@gmail.com>
2025-11-17 23:06:31 -0800

    Add clacks init CLI command

---
+++ src/slack_clacks/cli.py
@@ -0,0 +1,15 @@
+import argparse
+
+from slack_clacks.configuration.cli import generate_cli as generate_init_cli
+
+def generate_cli() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="clacks: Control Slack from your command line"
+    )
+    parser.set_defaults(func=lambda _: parser.print_help())
+    subparsers = parser.add_subparsers()
+
+    init_parser = generate_init_cli()
+    subparsers.add_parser("init", parents=[init_parser], add_help=False, help=init_parser.description)
+
+    return parser
---
+++ src/slack_clacks/configuration/cli.py
@@ -0,0 +1,17 @@
+import argparse
+from pathlib import Path
+
+from slack_clacks.configuration.database import ensure_db_initialized
+
+def handle_init(args: argparse.Namespace) -> None:
+    config_dir = args.config_dir
+    ensure_db_initialized(config_dir=config_dir)
+    print(f"Initialized configuration database at {config_dir}")
+
+def generate_cli() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="Manage clacks configuration databases"
+    )
+    parser.add_argument("-D", "--config-dir", type=Path, default=None, help="Directory in which to store the configuration database")
+    parser.set_defaults(func=handle_init)
+    return parser
